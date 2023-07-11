bd01b63826f2d97ee5e5a2f7a14de79954072940
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2023-07-11 13:15:19 -0500

    database exportada prueba

---
+++ database/bd_tasks_mysql.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Export to PHP Array plugin for PHPMyAdmin
+ * @version 5.2.1
+ */
+
+/**
+ * Database `bd_tasks_mysql`
+ */
+
+/* `bd_tasks_mysql`.`tasks` */
+$tasks = array(
+  array('id' => '1','title' => 'Create a website','description' => 'Create a website with Bootstrap 5!','created_at' => '2023-07-11 11:28:41'),
+  array('id' => '4','title' => 'abc','description' => 'abc abc abc','created_at' => '2023-07-11 11:49:07'),
+  array('id' => '5','title' => 'qwerty','description' => 'asdfghjk','created_at' => '2023-07-11 11:51:21'),
+  array('id' => '6','title' => 'Run','description' => 'I need to run!!!','created_at' => '2023-07-11 12:46:50'),
+  array('id' => '7','title' => 'Sleep','description' => 'I need to sleep!','created_at' => '2023-07-11 12:48:37'),
+  array('id' => '8','title' => 'Love','description' => 'I need to love!','created_at' => '2023-07-11 12:59:22')
+);
---
+++ database/bd_tasks_mysql.sql
@@ -0,0 +1,72 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 11-07-2023 a las 20:11:55
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bd_tasks_mysql`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tasks`
+--
+
+CREATE TABLE `tasks` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `tasks`
+--
+
+INSERT INTO `tasks` (`id`, `title`, `description`, `created_at`) VALUES
+(1, 'Create a website', 'Create a website with Bootstrap 5!', '2023-07-11 16:28:41'),
+(4, 'abc', 'abc abc abc', '2023-07-11 16:49:07'),
+(5, 'qwerty', 'asdfghjk', '2023-07-11 16:51:21'),
+(6, 'Run', 'I need to run!!!', '2023-07-11 17:46:50'),
+(7, 'Sleep', 'I need to sleep!', '2023-07-11 17:48:37'),
+(8, 'Love', 'I need to love!', '2023-07-11 17:59:22');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `tasks`
+--
+ALTER TABLE `tasks`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `tasks`
+--
+ALTER TABLE `tasks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
